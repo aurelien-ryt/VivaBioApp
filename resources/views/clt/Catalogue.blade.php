a892042cec03d46989164c017c50dcef9730a6d5
Sagbyy <sbouhdjeur.gagny@gmail.com>
2025-10-14 23:59:09 +0200

    Correction de la déclaration DOCTYPE et mise à jour des liens CSS dans la vue Catalogue

--- resources/views/clt/Catalogue.blade.php
+++ resources/views/clt/Catalogue.blade.php
@@ -1,4 +1,4 @@
-!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="fr">
   <head>
     <meta charset="UTF-8" />
@@ -6,10 +6,9 @@
     <title>VivaBio</title>
 
  
-    <link rel="stylesheet" href="src/style.css" />
-
-->
+    
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
+    @vite('resources/css/catalogue.css')
   </head>
 
   <body>
