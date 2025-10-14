8b0514522d2ee84d486baea02638b9111b8991ac
Sagbyy <sbouhdjeur.gagny@gmail.com>
2025-10-14 23:44:20 +0200

    Déplacement du projet dans la racine du repo

--- VivaBioApp/storage/framework/views/9dcd19e69f69eff70a839f765ece8c6c.php
+++
@@ -1,7 +0,0 @@
-<h1>Page de connexion</h1>
-<form method="POST" action="/Connexion">
-    <input type="email" name="email" placeholder="Email">
-    <input type="password" name="password" placeholder="Mot de passe">
-    <button type="submit">Se connecter</button>
-</form>
-<?php /**PATH /home/debian/VivaBioApp/VivaBioApp/resources/views/connexion/index.blade.php ENDPATH**/ ?>
--- VivaBioApp/storage/framework/views/c5eabbf7d5fdeeb45bec7e234dd5e673.php
+++
@@ -1,7 +0,0 @@
-<h1>Page d'inscription</h1>
-<form method="POST" action="/Inscription">
-    <input type="text" name="nom" placeholder="Nom">
-    <input type="email" name="email" placeholder="Email">
-    <button type="submit">S'inscrire</button>
-</form>
-<?php /**PATH /home/debian/VivaBioApp/VivaBioApp/resources/views/inscription/index.blade.php ENDPATH**/ ?>
